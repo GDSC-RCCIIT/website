39b486dfbb47c68b0483a31f67feabff22f23b9a
infuman69 <@gmail.com>
2022-10-13 13:25:07 +0530

    react-router upgraded

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,8 @@ import Team from "./pages/Team/Team";
 import Contact from "./pages/Contact/Contact";
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Redirect,
+  Routes,
 } from "react-router-dom";
 import { DatabaseProvider } from "./contexts/Database";
 import "./App.css";
@@ -20,18 +19,16 @@ export default function App() {
       <DatabaseProvider>
         <Router>
           <Navbar />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/event" exact component={Event} />
+          <Routes>
+            <Route index element={<Home/>} />
+            <Route path="/event" element={<Event/>} />
             <Route
               path="/speakers"
-              exact
-              component={Speakers}
+              element={<Speakers/>}
             />
-            <Route path="/team" exact component={Team} />
-            <Route path="/contact" exact component={Contact} />
-            <Redirect to="/" />
-          </Switch>
+            <Route path="/team" element={<Team/>} />
+            <Route path="/contact" element={<Contact/>} />
+          </Routes>
         </Router>
       </DatabaseProvider>
     </>
